0537dfc23face5efd9814b52103b3c820ec77bef
Juliano Boese <juliano.boese@gmail.com>
2022-05-17 17:57:15 +0000

    feat(req4): :sparkles: add validation to POST /login endpoint
    requirement 4 complete

--- routes/login.js
+++ routes/login.js
@@ -3,9 +3,23 @@ const randomstring = require('randomstring');
 
 const routes = express.Router();
 
-routes.post('/', (_req, res) => {
+routes.post('/', (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+
+  const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/igm;
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
+  }
+
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' }); 
+  }
+
   const token = randomstring.generate(16);
-  console.log(token);
   res.status(200).json({ token });
 });
 
