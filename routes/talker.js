04d99046e74ba916d879958d95c96fa5927b47f9
Juliano Boese <juliano.boese@gmail.com>
2022-05-18 14:17:23 +0000

    feat(req7): :sparkles: create the DELETE /talker/:id endpoint
    requirement 7 complete

--- routes/talker.js
+++ routes/talker.js
@@ -73,4 +73,18 @@ routes.put('/:id', validation, async (req, res) => {
   res.status(200).json(talkerData[talkerIndex]);
 });
 
+routes.delete('/:id', tokenValidation, async (req, res) => {
+  const { id } = req.params;
+
+  const talkerData = await readTalkerData();
+
+  const talkerIndex = talkerData.findIndex((talker) => talker.id === parseInt(id, 10));
+
+  talkerData.splice(talkerIndex, 1);
+
+  await writeTalkerData(talkerData);
+
+  res.status(204).end();
+});
+
 module.exports = routes;
