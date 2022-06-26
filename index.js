db95f1f51be569370285f4d0d76364811edff407
Juliano Boese <juliano.boese@gmail.com>
2022-06-26 16:53:46 -0300

    fix: :bug: fix swagger interfering in routes

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || 3000;
 
+app.use('/login', loginRoutes);
+app.use('/talker', talkerRoutes);
+
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
-
-app.use('/login', loginRoutes);
-app.use('/talker', talkerRoutes);
 
 app.listen(PORT, () => {
   console.log('Online');
